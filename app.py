c9e926ac10a845953f2058ec95184c0f917cf38f
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-05 01:26:10 +0300

    u

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.title('Test')
 # # Authenticate to Firestore with the JSON account key.
 # db = firestore.Client.from_service_account_json("firestore-key.json")
 
-
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="my-family-fund")
